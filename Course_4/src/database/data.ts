be40da692016c980342ebec950bfd54a448f3be3
YasiruWickramasinghe <it19038292@my.sliit.lk>
2023-08-15 09:17:49 +0530

    Crud created along with controller

--- Course_4/src/database/data.ts
+++ Course_4/src/database/data.ts
@@ -1,5 +1,35 @@
+export enum ReportType {
+    INCOME = "income",
+    EXPENSE = "expense"
+}
+
 export const data: Data = {
-    report: []
+    report: [
+        {
+            id: "uuid1",
+            source: "Fruit",
+            amount: 180000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        },
+        {
+            id: "uuid2",
+            source: "YouTube",
+            amount: 150000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.EXPENSE
+        },
+        {
+            id: "uuid3",
+            source: "Salary",
+            amount: 1750000,
+            created_at: new Date(),
+            updated_at: new Date(),
+            type: ReportType.INCOME
+        }
+    ]
 }
 
 interface Data {
@@ -9,20 +39,6 @@ interface Data {
         amount: number;
         created_at: Date;
         updated_at: Date;
-        type: "income" | "expense"
+        type: ReportType;
     }[]
 }
-
-enum ReportType {
-    INCOME = "income",
-    EXPENSE = "expense"
-}
-
-data.report.push({
-    id: "uuid",
-    source: "Salary",
-    amount: 180000,
-    created_at: new Date(),
-    updated_at: new Date(),
-    type: ReportType.INCOME
-})
